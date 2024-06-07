23aa610ff0aa9a8d06e362f001e90ae662d73cc0
ashishjat593 <ashishjat593@gmail.com>
2024-06-07 19:19:35 +0530

    ok

---
+++ C++ And Data Structures Algorithms  Hybrid/Lecture3.cpp
@@ -0,0 +1,5 @@
+// In this lecture, we covered the basics of variables, constants, and data types in C++:
+
+// Variables: These are used to store data that can change during program execution.
+// Constants: These hold data that cannot change once set.
+// Data Types: These define the kind of data a variable or constant can hold, like integers, floating-point numbers, and characters.
