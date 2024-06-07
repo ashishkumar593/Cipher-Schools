9507d67e3f89484c0a137081315dfcc41ae7ccbd
ashishjat593 <ashishjat593@gmail.com>
2024-06-07 19:00:26 +0530

    ok

--- C++ And Data Structures Algorithms  Hybrid/Lecture0.cpp
+++ C++ And Data Structures Algorithms  Hybrid/Lecture0.cpp
@@ -1,3 +1,4 @@
+//In this lecture ,Actually my mentor taught me about  Brief About The Course And Programming and he told that how about we patiance to the code .and this the Zero lecture for the Course of Data Structures using C++.there is no code introduction in this lecture so i didn't write. 
 #include <iostream>
 using namespace std;
 
---
+++ Lecture1.cpp
@@ -0,0 +1,6 @@
+//In our first lecture, we learned why C++ is great for studying data structures:
+
+//Efficiency: It's fast and handles heavy tasks well.
+// Strong Abstraction: It supports complex structures with features like classes.
+// STL: The Standard Template Library has useful pre-made structures and algorithms.
+// Compile-Time Checks: It catches errors early, making code more reliable.
