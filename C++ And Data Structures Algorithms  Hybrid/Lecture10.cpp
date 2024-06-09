e30d59ec851770c46c600d61cc534a92c2780630
ashishjat593 <ashishjat593@gmail.com>
2024-06-09 19:09:56 +0530

    ok

---
+++ C++ And Data Structures Algorithms  Hybrid/Lecture10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int i;
+//     for(i=1;i<100;i++){
+//         cout<<i<<" ";
+//     }
+// }
+
+// int main(){
+//     int i,j,k;
+//     i=0;
+//     j=0;
+//     k=0;
+
+//     cout<<i<<" "<<j<<" "<<k<<endl;
+//     j=i++;
+//     cout<<i<<" "<<j<<" "<<k<<endl;
+//     k=++i;
+// }
+
